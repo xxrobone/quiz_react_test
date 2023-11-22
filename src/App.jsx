6c02c5adc56d9adb7e55923f27f21549a7742514
robonexx <robertwagar@gmail.com>
2023-11-22 20:14:24 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import './App.scss';
-import Header from './components/header/Header';
-import Quiz from './components/quiz/Quiz';
 
 function App() {
   return (
     <div className='App'>
-      <Header title='Code Quiz' />
-      <h1>Are you ready?</h1>
-      <Quiz />
     </div>
   );
 }
